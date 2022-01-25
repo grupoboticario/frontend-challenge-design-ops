4fe912ff6045a2010a6b915fb9572f2c31f41dc4
Fernando Krasnievicz <ferpa.88@gmail.com>
2022-01-24 22:32:26 -0300

    feat: first commit
    . add stitches into the news-app project
    . created empy themes files into the DS project

--- packages/news-app/shared/components/button/index.tsx
+++ packages/news-app/shared/components/button/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from "next";
-
 import { styled } from '@stitches/react';
 
 const Button = styled('button', {
-  backgroundColor: 'gainsboro',
+  backgroundColor: 'red',
   borderRadius: '9999px',
   fontSize: '13px',
   padding: '10px 15px',
@@ -11,12 +9,3 @@ const Button = styled('button', {
     backgroundColor: 'lightgray',
   },
 });
-
-const Home: NextPage = () => {
-  return <>
-    <h1>Hello World!</h1>
-    <Button>Click me</Button>
-    </>;
-};
-
-export default Home;
