a5258b15c22ee96bcf92af049258a36547c00847
Rafael Barbosa Alves <rafael.alves.t_brq@grupoboticario.com.br>
2022-02-15 10:22:18 -0300

    refact(news-app/header): set flags default state and define cursor like pointer

--- packages/news-app/components/Header/Flags/index.tsx
+++ packages/news-app/components/Header/Flags/index.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 const Flags = () => {
   const { setTheme, resolvedTheme } = useTheme();
-  const [flags, setFlags] = useState([""]);
+  const [flags, setFlags] = useState<string[] | []>([]);
 
   useEffect(() => {
     setFlags(
@@ -28,6 +28,7 @@ const Flags = () => {
             alt={`${flag} flag`}
             src={`/${flag}.png`}
             onClick={() => handleTheme(flag)}
+            sx={{ cursor: "pointer" }}
           />
         );
       })}
